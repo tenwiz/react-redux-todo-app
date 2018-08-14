a53980680fbf9b7112c711d74410c52c47fc2e27
tenwiz <nethe0002@gmail.com>
2018-08-14 12:44:28 -0400

    Finished ToDo App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import styled from 'styled-components';
 
 import Header from './Header.jsx';
+import ToDoList from './ToDoList.jsx';
+const ContainerStyle = styled.div`
+  width: 400px;
+  margin: auto;
+  text-align: left;
+
+  @media (max-width: 700px) {
+    width: 100%;
+  }
+`;
 
 const App = ({ store }) => {
   return (
     <Provider store={store}>
-      <div>
+      <ContainerStyle>
         <Header/>
-        <div className="view">Some todos should be here</div>
-      </div>
+        <ToDoList />
+      </ContainerStyle>
     </Provider>
   );
 };
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,1 +1,95 @@
-export default (state = {}, action) => state;
+import {
+    TODO_FETCH_REQUEST,
+    TODO_FETCH_SUCCESS,
+    TODO_FETCH_ERROR,
+    TODO_ADD_REQUEST,
+    TODO_ADD_SUCCESS,
+    TODO_ADD_ERROR,
+    TODO_UPDATE_REQUEST,
+    TODO_UPDATE_SUCCESS,
+    TODO_UPDATE_ERROR,
+    TODO_REMOVE_REQUEST,
+    TODO_REMOVE_SUCCESS,
+    TODO_REMOVE_ERROR
+} from '../actionTypes';
+
+const initialState = {
+    todos: [],
+    isLoading: false,
+    error: null
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+      case TODO_FETCH_REQUEST:
+        return {
+          ...state,
+          error: null,
+          isLoading: true
+        };
+  
+      case TODO_FETCH_SUCCESS:
+        return {
+          ...state,
+          error: null,
+          isLoading: false,
+          todos: action.data
+        };
+  
+      case TODO_ADD_REQUEST:
+        return {
+          ...state,
+          error: null,
+          isLoading: true
+        };
+  
+      case TODO_ADD_SUCCESS:
+        return {
+          ...state,
+          error: null,
+          isLoading: false
+        };
+  
+      case TODO_UPDATE_REQUEST:
+        return {
+          ...state,
+          error: null,
+          isLoading: true
+        };
+  
+      case TODO_UPDATE_SUCCESS:
+        return {
+          ...state,
+          error: null,
+          isLoading: false
+        };
+  
+      case TODO_REMOVE_REQUEST:
+        return {
+          ...state,
+          error: null,
+          isLoading: true
+        };
+  
+      case TODO_REMOVE_SUCCESS:
+        return {
+          ...state,
+          error: null,
+          isLoading: false
+        };
+  
+      case TODO_FETCH_ERROR:
+      case TODO_ADD_ERROR:
+      case TODO_UPDATE_ERROR:
+      case TODO_REMOVE_ERROR:
+        return {
+          ...state,
+          isLoading: false,
+          error: action.error
+        };
+  
+      default:
+        return state;
+    }
+};
+  
